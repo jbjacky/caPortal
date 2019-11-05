866f235a333e8e357d88864a5b677b17ba7f4fd2
jackypc183 <jackypc183@gmail.com>
2019-11-05 12:09:43 +0800

    v2.0.2-設定檔分離

---
+++ src/assets/setting46.js
@@ -0,0 +1,16 @@
+
+var desktopTitleString = '國內(含外派)員工差假系統 (限TT/FF/LD輪班)'
+
+var phoneTitleString = '國內(含外派)員工差假系統 (限TT/FF/LD輪班)'
+
+var footerString = 'copyright: China Airlines 2019 (測試環境 Test Environment)'
+
+var OAuthPath = 'https://192.168.1.46/OAuth20/authorize.aspx'
+
+var _client_id = 't4Ev4xhBABswPSkXIwBn'
+
+var _redirect_uri_LinkOuthpage = 'https://192.168.1.46/EAM/LinkOuthpageComponent'
+
+var apiPostURL = 'https://192.168.1.46/eepWebService/'
+
+var apiGetFileURL = 'https://192.168.1.46/eepWebServiceExtend/'
---
+++ src/assets/settingAll.js
@@ -0,0 +1,29 @@
+
+    var QAhref = 'https://hdqhr05t.china-airlines.com/eepWebServiceExtend/Upload/EAM_QA.pdf'
+    
+    var manualhref = 'https://hdqhr05t.china-airlines.com/eepWebServiceExtend/Upload/EAM_Manual.pdf'
+
+    var TimeOut = '20000'
+
+    var OAuthPath = 'https://iam.china-airlines.com/mga/sps/oauth/oauth20/authorize'
+
+    var _client_id = 't4Ev4xhBABswPSkXIwBn'
+
+    var FileUploadType = [
+        ".doc",
+        ".docx",
+        ".ppt",
+        ".pptx",
+        ".xls",
+        ".xlsx",
+        ".jpg",
+        ".txt",
+        ".odt",
+        ".odp",
+        ".ods",
+        ".htm",
+        ".html",
+        ".mht",
+        ".pdf",
+        ".jpeg"
+      ]
