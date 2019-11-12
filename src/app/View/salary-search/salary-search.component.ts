074199830eaa6968fa588b91b2e34a744b2425b5
jackypc183 <jackypc183@gmail.com>
2019-11-12 08:43:22 +0800

    JB-v2.0.2-薪資單查詢畫面未完成

--- src/app/View/salary-search/salary-search.component.ts
+++ src/app/View/salary-search/salary-search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { interval } from 'rxjs';
+import { map, takeWhile, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-salary-search',
@@ -9,7 +12,32 @@ export class SalarySearchComponent implements OnInit {
 
   constructor() { }
 
+  time = 5
+  get getTime() { return this.time }
+  resetTime() {
+    this.time = 5
+  }
+  time_subscribe = true;
   ngOnInit() {
+    interval(1000)
+    .pipe(
+      map((x) => {
+        if(this.time>1){
+          this.time = this.time - 1
+        }else if(this.time == 1){
+          this.time = this.time - 1
+          console.log('執行')
+        }
+      })
+    ).toPromise();
   }
 
+  firstPwFromGroup: FormGroup = new FormGroup({
+    empID: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    checkPassword: new FormControl('', Validators.required)
+  })
+  submit() {
+    console.log()
+  }
 }
