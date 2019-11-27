e0e3b72ceadfde5c875c526298bafeaf13177025
jackypc182 <jackypc182@gmail.com>
2019-11-27 14:10:11 +0800

    v2.0.5-註記單審核查詢修改中(修正審核請假銷假填寫人)

--- src/app/Models/NewVaSearchFlowSignClass.ts
+++ src/app/Models/NewVaSearchFlowSignClass.ts
@@ -1,12 +1,12 @@
 
 export class NewVaSearchFlowSignClass {
-  uiHolidayName:Array<string>;
+  uiHolidayName:Array<any>;
   ProcessFlowID: number
   showProcessFlowID: number
   EmpCode: string
   EmpNameC: string
   AppDeptName: string
-  State: String
+  State: string
   ManageEmpName: string
   HoliDayID: number
   HoliDayNameC: number
@@ -29,8 +29,8 @@ export class NewVaSearchFlowSignClass {
   hour: string
   minute: string
 
-  key: String
-  OldKey: String
+  key: string
+  OldKey: string
 }
 export class DetailNewVaSearchFlowSignClass {
   ProcessFlowID: number
