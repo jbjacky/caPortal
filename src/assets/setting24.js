52ebae5f2df59f384a6e49b7557eb007ce5fea54
jackypc182 <jackypc182@gmail.com>
2019-12-03 17:32:22 +0800

    JB-v2.0.2-上24版本

---
+++ src/assets/setting24.js
@@ -0,0 +1,19 @@
+var QAhref = ''
+
+var manualhref = ''
+
+var desktopTitleString = '員工差假系統'
+
+var phoneTitleString = '員工差假系統'
+
+var footerString = ''
+
+var OAuthPath = ''
+
+var _client_id = ''
+
+var _redirect_uri_LinkOuthpage = ''
+
+var apiPostURL = 'http://publish.jbjob.com.tw/HRWebService/api/'
+
+var apiGetFileURL = ''
