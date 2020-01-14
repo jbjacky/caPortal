7976eae89a9a18ed7cfa2d80e16ee94803c47217
映呈 劉 <jacky@jbjob.com.tw>
2020-01-14 11:38:59 +0800

    v2.1.6-24測試版

---
+++ src/assets/setting24.js
@@ -0,0 +1,19 @@
+var QAhref = ''
+    
+var manualhref = ''
+
+var desktopTitleString = '差假系統'
+
+var phoneTitleString = '差假系統'
+
+var footerString = '(測試環境 Test Environment)'
+
+var OAuthPath = 'http://publish.jbjob.com.tw/DEMO/OAuth20/authorize.aspx'
+
+var _client_id = 't4Ev4xhBABswPSkXIwBn'
+
+var _redirect_uri_LinkOuthpage = 'http://publish.jbjob.com.tw/EAM/LinkOuthpageComponent'
+
+var apiPostURL = 'http://publish.jbjob.com.tw/DEMO/eepwebservice/'
+
+var apiGetFileURL = 'http://publish.jbjob.com.tw/DEMO/eepWebServiceExtend/'
